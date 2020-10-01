9eb194ede82e5bd681b979508e04d391962f3387
brian-duckworth <bcduckworth@gmail.com>
2020-10-01 17:03:05 -0400

    Third comment.

--- hello_world.R
+++ hello_world.R
@@ -1,3 +1,4 @@
 # Adding comments just to make changes for the GitHub tutorial.
 # Adding a second comment in GitHub to practice pull from RStudio.
+# Adding a third comment to RStudio to practice push from Rstudio.
 print("Hello world!")
